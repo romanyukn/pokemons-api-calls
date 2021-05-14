444d1f48de2e11c00b20eac3bfb62c30228b0496
Natalia Romaniuk <romanyukn@gmail.com>
2021-05-14 14:58:53 -0400

    fix the api calls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PokemonList from './PokemonList';
+import './App.css';
 
 function App() {
   return (
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,33 +1,50 @@
 import React, { useEffect, useState } from 'react';
+import { ListGroup } from "react-bootstrap"
 import axios from 'axios';
 
 function PokemonList() {
   const [pokemons, setPokemons] = useState([]);
 
-  useEffect(async() => {
-    const result = await axios.get('https://pokeapi.co/api/v2/pokemon');
-    const pokemonsRes = result.data.results;
-    const pokemonsArr = [];
-    pokemonsRes.forEach(async(el) => {
-      try {
-        const pokemonObj = {};
-        const pokemonInfo = await axios.get(el.url);
-        pokemonObj.name = el.name;
-        pokemonObj.img = pokemonInfo.data.sprites.front_default;
-        pokemonObj.type = pokemonInfo.data.types.map(type => type.type.name);
-        pokemonsArr.push(pokemonObj); 
-      }
-      catch(e){
-        console.log(e);
-      }
-    })
-    setPokemons(pokemonsArr);
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios.get('https://pokeapi.co/api/v2/pokemon');
+      const pokemons = result.data.results;
+
+      const promises = pokemons.map((pokemon) =>{
+        return axios.get(pokemon.url);
+      })
+
+      const pokemonInfo = (await Promise.all(promises))
+        .map((result) => {
+          return result.data
+        })
+        .map((info) => {
+          return {
+            id: info.id,
+            name: info.name,
+            image: info.sprites.front_default,
+            types: info.types.map(el => el.type.name)
+          }
+       })
+       
+      setPokemons(pokemonInfo);
+    }
+    fetchData();
   }, [])
 
   return(
-    <ul>
-      {pokemons.map(el => <li>{el.name} {el.img}</li>)}
-    </ul>
+    <div>
+      <h3 className="header">Pokemons API</h3>
+      {
+        pokemons.map(el => (
+          <ListGroup key={el.id}>
+            <ListGroup.Item style={{marginRight: 200, marginLeft: 200, marginTop: 20}}>
+              <h3><img src={el.image} alt=''></img> {el.name} - {el.types.join(', ')}</h3>
+            </ListGroup.Item>
+          </ListGroup>
+        ))
+      }
+    </div>
   )
 }
 
